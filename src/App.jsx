5ca7c80f4570606c0f8a03083b3eabbc95ddc434
ARDA KOCAK <kocakarda640@gmail.com>
2026-05-06 08:41:36 +0000

    Gelişmiş STL Analiz Sistemi

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ const AuthenticatedApp = () => {
   if (authError) {
     if (authError.type === 'user_not_registered') {
       return <UserNotRegisteredError />;
+    } else if (authError.type === 'auth_required') {
+      navigateToLogin();
+      return null;
     }
   }
 
