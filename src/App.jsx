4de6e3eadbf81e9a9425a5ba6dd63a84c392aac6
ARDA KOCAK <kocakarda640@gmail.com>
2026-05-06 08:40:19 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,6 @@ const AuthenticatedApp = () => {
   if (authError) {
     if (authError.type === 'user_not_registered') {
       return <UserNotRegisteredError />;
-    } else if (authError.type === 'auth_required') {
-      navigateToLogin();
-      return null;
     }
   }
 
